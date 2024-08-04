24722b7c75fe197fe9ec2931911753f50cf04ef4
[aandresis99] <[andresis.1999@gmail.com]>
2024-08-04 00:07:08 -0500

    Se añaden variables de entorno

--- app.js
+++ app.js
@@ -13,11 +13,16 @@ const validar = require('./validar');
 const saltRounds = 10;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.URLFRONTEND || 'http://localhost:5173',
     credentials: true
 }))
 app.use(session({
-    secret: "prgjwpldgpsdgñlsdpgojwrpbhjldnbñsdnposnjdpl"
+    secret: process.env.SECRETSESSION || "prgjwpldgpsdgñlsdpgojwrpbhjldnbñsdnposnjdpl",
+    proxy: process.env.NODE_ENV === 'production',
+        cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none'
+    }
 }))
 
 app.get('/', (req, res) => {
